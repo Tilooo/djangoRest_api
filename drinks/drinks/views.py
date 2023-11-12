d2d2393ad8b3b9392f1c5d63c2847fbc948b385f
Tilooo <moonaviad@gmail.com>
2023-11-12 16:34:16 +0200

    Created DELETE, for to delete data.

--- drinks/drinks/views.py
+++ drinks/drinks/views.py
@@ -45,8 +45,10 @@ def drink_detail(request, id):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     elif request.method == 'DELETE':
-        pass
+        drink.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 
